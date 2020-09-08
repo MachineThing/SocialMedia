cbed901468012ea95238eba567b5c24f27ba9467
Mason Fisher <sackzombie10@gmail.com>
2020-09-08 14:12:49 -0400

    Using PostgreSQL

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'postgres',
+        'PASSWORD': 'apD696969',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
