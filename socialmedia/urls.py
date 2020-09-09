50da87ba65b02db9feb6f31f880a8e8b50e93db3
Mason Fisher <sackzombie10@gmail.com>
2020-09-09 10:53:38 -0400

    Made views for the login and register pages

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views
 
 urlpatterns = [
-    path('', include('users.urls')),
+    path('', views.homepage, name="homepage"),
+    path('login/', views.login, name="login"),
+    path('register/', views.login, name="register"),
+    path('user/', include('users.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/views.py
+++ users/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'users/index.html')
+
+def login(request):
+    return render(request, 'users/login.html')
+
+def register(request):
+    return render(request, 'users/login.html')
