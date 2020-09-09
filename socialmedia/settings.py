5696ed295a267545cdf4835e1869b5551c62fdca
Mason Fisher <sackzombie10@gmail.com>
2020-09-09 10:42:03 -0400

    Using templates

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'socialmedia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR+'/socialmedia/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
